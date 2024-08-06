989dcc0c65703402123833e3fa0c9ea6f814c4c6
ahmadullah <nikzadahmadullah@gmail.com>
2024-08-06 14:26:12 +0200

    max-w changed

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -2,7 +2,7 @@
 
 export function Container({children}:{children:React.ReactNode}){
     return(
-        <div className='mx-auto max-w-7xl'>
+        <div className='mx-auto'>
             {children}
         </div>
     )
