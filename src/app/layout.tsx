245bf46d13d8c9cf8efe875e77ed60aa31edaee8
ahmadullah <nikzadahmadullah@gmail.com>
2024-08-06 14:31:43 +0200

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/footer";
@@ -26,6 +27,7 @@ export default function RootLayout({
         />
         <Navbar/>
         {children}
+        <Analytics />
         <Footer/>
         </body>
     </html>
